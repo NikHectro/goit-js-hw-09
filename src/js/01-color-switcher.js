a292c63cad2313451f213b1d089f6c13d794eed6
NikHectro <99733904+NikHectro@users.noreply.github.com>
2022-08-13 22:47:00 +0300

    Task 1 + npw init

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const body = document.body;
+let timerId = null;
+
+stopBtn.setAttribute("disabled", true);
+
+startBtn.addEventListener('click', () => {
+    startBtn.setAttribute("disabled", true);
+    stopBtn.removeAttribute("disabled");
+    timerId = setInterval(onStart, 1000)
+});
+stopBtn.addEventListener('click', onStop);
+
+function onStart() {
+    body.style.backgroundColor = getRandomHexColor();
+}
+
+function onStop() {
+    startBtn.removeAttribute("disabled")
+    stopBtn.setAttribute("disabled", true);
+    clearInterval(timerId)
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
